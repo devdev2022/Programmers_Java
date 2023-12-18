31f363cd6cd58f650516ea31a57137a520b4f7df
Hansol Kim <100466989+devdev2022@users.noreply.github.com>
2023-12-18 16:09:33 +0900

    [level unrated] Title: 옷가게 할인 받기, Time: 0.02 ms, Memory: 71.4 MB -BaekjoonHub

---
+++ 프로그래머스/unrated/120818. 옷가게 할인 받기/옷가게 할인 받기.java
@@ -0,0 +1,14 @@
+class Solution {
+    public int solution(int price) {
+        if(price>=100000&& price<300000) {
+            return (int)(0.95*price);
+        }
+        else if(price>=300000&& price<500000) {
+            return (int)(0.9*price);
+        }
+        else if(price>=500000) {
+            return (int)(0.8*price);
+        }
+        else return price;
+    }
+}
